05f905ff8ce4578e6e9f3c20de2c2f37eb4c9d8a
xenotaph <91130260+xenotaph@users.noreply.github.com>
2024-02-23 09:54:46 +0100

    Add Change Password link to menu

--- menu.php
+++ menu.php
@@ -8,25 +8,29 @@ if (session_status() == PHP_SESSION_NONE) {
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="index.php">RQG</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+	<span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <?php if (isset($_SESSION['username'])): ?>
-        <li class="nav-item">
-          <a class="nav-link" href="new_character.php">New Character</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="load_character.php">Load Character</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="logout.php">Logout</a>
-        </li>
-      <?php else: ?>
-        <li class="nav-item">
-          <a class="nav-link" href="login.php">Login</a>
-        </li>
-      <?php endif; ?>
-    </ul>
+	<ul class="navbar-nav">
+	    <?php if (isset($_SESSION['username'])): ?>
+		<li class="nav-item">
+		  <a class="nav-link" href="new_character.php">New Character</a>
+		</li>
+		<li class="nav-item">
+		  <a class="nav-link" href="load_character.php">Load Character</a>
+		</li>
+		<li class="nav-item">
+		  <a class="nav-link" 
+	    <li class="nav-item">
+		    <a class="nav-link" href="change_password.php">Change Password</a>
+		  </li>
+		  href="logout.php">Logout</a>
+		</li>
+	  <?php else: ?>
+		<li class="nav-item">
+		  <a class="nav-link" href="login.php">Login</a>
+		</li>
+	  <?php endif; ?>
+	</ul>
   </div>
 </nav>
